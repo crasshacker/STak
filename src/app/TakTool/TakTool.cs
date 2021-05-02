881f8998cdcd51bccd29c44f8c947b0ce9af1ff0
Scott Southard <scott.southard@gmail.com>
2021-05-02 18:39:08 -0400

    Use System.CommandLine for cmdline parsing.

--- src/app/TakTool/TakTool.cs
+++ src/app/TakTool/TakTool.cs
@@ -1,36 +1,198 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Diagnostics;
-using CommandLine;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 using STak.TakEngine;
 using STak.TakEngine.AI;
 using STak.TakEngine.Extensions;
 
 namespace STak.TakTool
 {
-    public class Program
+    class Program
     {
-        static int Main(string[] args)
+        private static Option[] MoveOptions = new Option[]
         {
-            var result = Parser.Default.ParseArguments<MoveOptions, BattleOptions>(args)
-                                       .WithParsed<MoveOptions>(options   => ProcessMoveCommand(options))
-                                       .WithParsed<BattleOptions>(options => ProcessBattleCommand(options))
-                                       .WithNotParsed(errors              => ProcessErrors(errors));
+            new Option<string> ("--ai-name",          () => "Dinkum Thinkum",     "The name of the AI to make the move."),
+            new Option<int>    ("--ai-search-depth",  () => 4,                    "The search depth used by the AI."),
+            new Option<int>    ("--ai-thinking-time", () => 0,                    "The maximum per-move thinking time."),
+            new Option<int>    ("--board-size",       () => 5,                    "The size of the game board."),
+            new Option<bool>   ("--verbose",          () => false,                "Produce verbose output.")
+        };
 
-            return result is NotParsed<object> ? 1 : 0;
+        private static Option[] BattleOptions = new Option[]
+        {
+            new Option<string> ("--name",              () => "DT_vs_TE",           "The name to assign the battle."),
+
+            new Option<string> ("--ai1-name",          () => "Dinkum Thinkum",     "Name of player 1 AI."),
+            new Option<int>    ("--ai1-search-depth",  () => 4,                    "Search depth used by player 1 AI."),
+            new Option<int>    ("--ai1-thinking-time", () => 0,                    "Maximum thinking time for player 1 AI."),
+
+            new Option<string> ("--ai2-name",          () => "The Experiment",     "Name of player 2 AI."),
+            new Option<int>    ("--ai2-search-depth",  () => 4,                    "Search depth used by player 2 AI."),
+            new Option<int>    ("--ai2-thinking-time", () => 0,                    "Maximum thinking time for player 1 AI."),
+
+            new Option<int>    ("--board-size",        () => 5,                    "Size of the game board."),
+            new Option<int>    ("--game-count",        () => 1,                    "Number of games in the competition."),
+            new Option<int>    ("--random-seed",       () => 0,                    "Random number generator seed value."),
+            new Option<bool>   ("--force",             () => false,                "Force overwrite of existing result files."),
+            new Option<bool>   ("--verbose",           () => false,                "Produce verbose output.")
+        };
+
+
+        static void Main(string[] args)
+        {
+            var root   = new RootCommand("root");
+            var battle = new Command("battle");
+            var move   = new Command("move");
+
+            foreach (var option in BattleOptions) { battle.Add(option); }
+            foreach (var option in MoveOptions  ) { move  .Add(option); }
+
+            battle.Handler = CommandHandler.Create((BattleOptionsBag options) => DoBattle(options));
+            move  .Handler = CommandHandler.Create((MoveOptionsBag   options) => DoMove  (options));
+
+            root.Add(battle);
+            root.Add(move);
+
+            root.Handler = CommandHandler.Create(() => {});
+            root.Invoke(args);
         }
 
 
-        private static void ProcessMoveCommand(MoveOptions options)
+        private static void DoBattle(BattleOptionsBag options)
         {
+            // PrintProperties(options, BindingFlags.Public | BindingFlags.Instance);
+
+            var battleName         = options.Name;
+
+            var ai1Name            = options.AI1Name;
+            var ai1SearchDepth     = options.AI1SearchDepth;
+            var ai1MaxThinkingTime = options.AI1ThinkingTime;
+
+            var ai2Name            = options.AI2Name;
+            var ai2SearchDepth     = options.AI2SearchDepth;
+            var ai2MaxThinkingTime = options.AI2ThinkingTime;
+
+            var boardSize          = options.BoardSize;
+            var gameCount          = options.GameCount;
+
+            var seed               = options.RandomSeed;
+            var force              = options.Force;
+            var verbose            = options.Verbose;
+
+            VerboseWriter.Verbose = verbose;
+
+            TakAI.LoadPlugins();
+
+            string battleDir = battleName;
+
+            var ai1 = TakAI.GetAI(ai1Name);
+            var ai2 = TakAI.GetAI(ai2Name);
+
+            ai1.Options = AIConfiguration<TakAIOptions>.Get(ai1Name);
+            ai2.Options = AIConfiguration<TakAIOptions>.Get(ai2Name);
+
+            if (ai1SearchDepth > 0) { ai1.Options.TreeEvaluationDepth = ai1SearchDepth; }
+            if (ai2SearchDepth > 0) { ai2.Options.TreeEvaluationDepth = ai2SearchDepth; }
+
+            // Negative values signify (positive) milliseconds; positive values signify seconds; zero means infinite.
+            // Note: -1 is equivalent to Timeout.InfiniteTimeSpan, used in the .NET Core timer code.
+
+            ai1MaxThinkingTime = (ai1MaxThinkingTime > 0) ?  ai1MaxThinkingTime * 1000
+                               : (ai1MaxThinkingTime < 0) ? -ai1MaxThinkingTime
+                                                          : -1;
+            ai2MaxThinkingTime = (ai2MaxThinkingTime > 0) ?  ai2MaxThinkingTime * 1000
+                               : (ai2MaxThinkingTime < 0) ? -ai2MaxThinkingTime
+                                                          : -1;
+
+            ai1.Options.MaximumThinkingTime = ai1MaxThinkingTime;
+            ai2.Options.MaximumThinkingTime = ai2MaxThinkingTime;
+
+            ai1.Options.RandomizationSeed = seed;
+            ai2.Options.RandomizationSeed = seed;
+
+            if (seed != 0)
+            {
+                ai1.Options.EvaluateMovesInParallel = false;
+                ai2.Options.EvaluateMovesInParallel = false;
+                ai1.Options.EvaluateCellsRandomly   = false;
+                ai2.Options.EvaluateCellsRandomly   = false;
+            }
+
+            var player1 = new Player(ai1Name, ai1);
+            var player2 = new Player(ai2Name, ai2);
+
+            Stopwatch stopwatch = new Stopwatch();
+            var battle = new Battle(battleName, battleDir, ai1Name, ai2Name, force);
+
+            Stopwatch[] stopwatches = { new Stopwatch(), new Stopwatch() };
+
+            VerboseWriter.Write($"Starting {gameCount} game battle on {boardSize}x{boardSize} board:"
+                                                                       + $" {ai1Name} vs. {ai2Name}.");
+
+            for (int gameNumber = 1; gameNumber <= gameCount; ++gameNumber)
+            {
+                var prototype = new GamePrototype(player1, player2, boardSize);
+                var game      = new BasicGame(prototype);
+
+                var moveRecords = new List<MoveRecord>();
+
+                VerboseWriter.Write($"Starting game {gameNumber}/{gameCount} - {ai1Name} vs. {ai2Name}.");
+
+                game.Initialize();
+                game.Start();
+
+                for (int ply = 0; ! game.IsCompleted; ply++)
+                {
+                    int playerId = ply % 2;
+                    var ai = playerId == 0 ? ai1 : ai2;
+                    var start = stopwatches[playerId].Elapsed;
+                    stopwatches[playerId].Start();
+
+                    int timeout = (playerId == Player.One) ? ai1MaxThinkingTime : ai2MaxThinkingTime;
+                    using var cancelTokenSource = new CancellationTokenSource(timeout);
+                    var move = ai.ChooseNextMove(game, cancelTokenSource.Token);
+
+                    var elapsed = stopwatches[playerId].Elapsed - start;
+                    bool cancelled = cancelTokenSource.IsCancellationRequested;
+                    var moveRecord = new MoveRecord(ply, move, elapsed, cancelled);
+                    VerboseWriter.Write(moveRecord.ToString());
+                    moveRecords.Add(moveRecord);
+
+                    game.MakeMove(move);
+                    stopwatches[playerId].Stop();
+                }
+
+                var ai1Time = stopwatches[0].Elapsed;
+                var ai2Time = stopwatches[1].Elapsed;
+
+                string ptn = PtnParser.ToString(game, "TakTool", false, true, true);
+                var result = new BattleResult(ai1Name, ai1Time, ai2Name, ai2Time, ptn, moveRecords);
+                battle.AddResult(result);
+                battle.Save();
+
+                stopwatches[0].Reset();
+                stopwatches[1].Reset();
+
+                VerboseWriter.Write($"\nGame {gameNumber} results:\n{result}\n");
+            }
+        }
+
+
+        private static void DoMove(MoveOptionsBag options)
+        {
+            // PrintProperties(options, BindingFlags.Public | BindingFlags.Instance);
+
             var aiName          = options.AIName;
             var searchDepth     = options.SearchDepth;
-            var maxThinkingTime = options.MaxThinkingTime;
+            var maxThinkingTime = options.ThinkingTime;
             var boardState      = options.BoardState;
             var verbose         = options.Verbose;
 
@@ -91,140 +253,21 @@ namespace STak.TakTool
         }
 
 
-        private static void ProcessBattleCommand(BattleOptions options)
+        private static void PrintProperties(object options, BindingFlags bindingFlags)
         {
-            var battleName         = options.Name;
+            var maximumLength = 0;
 
-            var ai1Info            = options.AI1;
-            var ai1Name            = options.AI1Name;
-            var ai1SearchDepth     = options.AI1SearchDepth;
-            var ai1MaxThinkingTime = options.AI1MaxThinkingTime;
-
-            var ai2Info            = options.AI2;
-            var ai2Name            = options.AI2Name;
-            var ai2SearchDepth     = options.AI2SearchDepth;
-            var ai2MaxThinkingTime = options.AI2MaxThinkingTime;
-
-            var boardSize          = options.BoardSize;
-            var gameCount          = options.GameCount;
-
-            var seed               = options.Seed;
-            var force              = options.Force;
-            var verbose            = options.Verbose;
-
-            VerboseWriter.Verbose = verbose;
-
-            TakAI.LoadPlugins();
-
-            string battleDir = battleName;
-
-            if (ai1Info != null)
+            foreach (var property in options.GetType().GetProperties(bindingFlags))
             {
-                var parts = Regex.Split(ai1Info, @"\s*,\s*");
-                if (parts.Length != 3) { Barf("Invalid ai1 option value."); }
-                ai1Name            = parts[0];
-                ai1SearchDepth     = Int32.Parse(parts[1]);
-                ai1MaxThinkingTime = Int32.Parse(parts[2]);
+                maximumLength = Math.Max(maximumLength, property.Name.Length);
             }
 
-            if (ai2Info != null)
+            foreach (var property in options.GetType().GetProperties(bindingFlags))
             {
-                var parts = Regex.Split(ai2Info, @"\s*,\s*");
-                if (parts.Length != 3) { Barf("Invalid ai2 option value."); }
-                ai2Name            = parts[0];
-                ai2SearchDepth     = Int32.Parse(parts[1]);
-                ai2MaxThinkingTime = Int32.Parse(parts[2]);
-            }
+                var name  = property.Name;
+                var value = property.GetValue(options);
 
-            var ai1 = TakAI.GetAI(ai1Name);
-            var ai2 = TakAI.GetAI(ai2Name);
-
-            ai1.Options = AIConfiguration<TakAIOptions>.Get(ai1Name);
-            ai2.Options = AIConfiguration<TakAIOptions>.Get(ai2Name);
-
-            if (ai1SearchDepth > 0) { ai1.Options.TreeEvaluationDepth = ai1SearchDepth; }
-            if (ai2SearchDepth > 0) { ai2.Options.TreeEvaluationDepth = ai2SearchDepth; }
-
-            // Negative values signify (positive) milliseconds; positive values signify seconds; zero means infinite.
-            // Note: -1 is equivalent to Timeout.InfiniteTimeSpan, used in the .NET Core timer code.
-
-            ai1MaxThinkingTime = (ai1MaxThinkingTime > 0) ?  ai1MaxThinkingTime * 1000
-                               : (ai1MaxThinkingTime < 0) ? -ai1MaxThinkingTime
-                                                          : -1;
-            ai2MaxThinkingTime = (ai2MaxThinkingTime > 0) ?  ai2MaxThinkingTime * 1000
-                               : (ai2MaxThinkingTime < 0) ? -ai2MaxThinkingTime
-                                                          : -1;
-
-            ai1.Options.MaximumThinkingTime = ai1MaxThinkingTime;
-            ai2.Options.MaximumThinkingTime = ai2MaxThinkingTime;
-
-            ai1.Options.RandomizationSeed = seed;
-            ai2.Options.RandomizationSeed = seed;
-
-            if (seed != 0)
-            {
-                ai1.Options.EvaluateMovesInParallel = false;
-                ai2.Options.EvaluateMovesInParallel = false;
-                ai1.Options.EvaluateCellsRandomly   = false;
-                ai2.Options.EvaluateCellsRandomly   = false;
-            }
-
-            var player1 = new Player(ai1Name, ai1);
-            var player2 = new Player(ai2Name, ai2);
-
-            Stopwatch stopwatch = new Stopwatch();
-            var battle = new Battle(battleName, battleDir, ai1Name, ai2Name,force);
-
-            Stopwatch[] stopwatches = { new Stopwatch(), new Stopwatch() };
-
-            VerboseWriter.Write($"Starting {gameCount} game battle on {boardSize}x{boardSize} board:"
-                                                                       + $" {ai1Name} vs. {ai2Name}.");
-
-            for (int gameNumber = 1; gameNumber <= gameCount; ++gameNumber)
-            {
-                var prototype = new GamePrototype(player1, player2, boardSize);
-                var game      = new BasicGame(prototype);
-
-                var moveRecords = new List<MoveRecord>();
-
-                VerboseWriter.Write($"Starting game {gameNumber}/{gameCount} - {ai1Name} vs. {ai2Name}.");
-
-                game.Initialize();
-                game.Start();
-
-                for (int ply = 0; ! game.IsCompleted; ply++)
-                {
-                    int playerId = ply % 2;
-                    var ai = playerId == 0 ? ai1 : ai2;
-                    var start = stopwatches[playerId].Elapsed;
-                    stopwatches[playerId].Start();
-
-                    int timeout = (playerId == Player.One) ? ai1MaxThinkingTime : ai2MaxThinkingTime;
-                    using var cancelTokenSource = new CancellationTokenSource(timeout);
-                    var move = ai.ChooseNextMove(game, cancelTokenSource.Token);
-
-                    var elapsed = stopwatches[playerId].Elapsed - start;
-                    bool cancelled = cancelTokenSource.IsCancellationRequested;
-                    var moveRecord = new MoveRecord(ply, move, elapsed, cancelled);
-                    VerboseWriter.Write(moveRecord.ToString());
-                    moveRecords.Add(moveRecord);
-
-                    game.MakeMove(move);
-                    stopwatches[playerId].Stop();
-                }
-
-                var ai1Time = stopwatches[0].Elapsed;
-                var ai2Time = stopwatches[1].Elapsed;
-
-                string ptn = PtnParser.ToString(game, "TakTool", false, true, true);
-                var result = new BattleResult(ai1Name, ai1Time, ai2Name, ai2Time, ptn, moveRecords);
-                battle.AddResult(result);
-                battle.Save();
-
-                stopwatches[0].Reset();
-                stopwatches[1].Reset();
-
-                VerboseWriter.Write($"\nGame {gameNumber} results:\n{result}\n");
+                Console.WriteLine($"{name.PadRight(maximumLength)} => {value}");
             }
         }
 
@@ -417,76 +460,38 @@ namespace STak.TakTool
         }
 
 
-        private static void ProcessErrors(IEnumerable<Error> errors)
+        private class OptionsBag
         {
-            // No action needed - errors are printed by default.`
         }
 
 
-        [Verb("move", HelpText="Choose a good move, print the PTN to stdout.")]
-        public class MoveOptions
+        private class MoveOptionsBag : OptionsBag
         {
-            [Option(Default="Dinkum Thinkum")]
-            public string AIName { get; set; }
-
-            [Option(Default=3)]
-            public int SearchDepth { get; set; }
-
-            [Option(Default=0)]
-            public int MaxThinkingTime { get; set; }
-
-            [Option(Required=true)]
-            public string BoardState { get; set; }
-
-            [Option(Default=false)]
-            public bool Verbose { get; set; }
+            public string AIName       { get; set; }
+            public int    SearchDepth  { get; set; }
+            public int    ThinkingTime { get; set; }
+            public string BoardState   { get; set; }
+            public bool   Verbose      { get; set; }
         }
 
 
-        [Verb("battle", HelpText="Battle two AIs against one another in a series of games.")]
-        public class BattleOptions
+        private class BattleOptionsBag : OptionsBag
         {
-            [Option(Required=true)]
-            public string Name { get; set; }
+            public string Name            { get; set; }
 
-            [Option(SetName="shorthand", Default=null)]
-            public string AI1 { get; set; }
+            public string AI1Name         { get; set; }
+            public int    AI1SearchDepth  { get; set; }
+            public int    AI1ThinkingTime { get; set; }
 
-            [Option(SetName="longhand", Default="Dinkum Thinkum")]
-            public string AI1Name { get; set; }
+            public string AI2Name         { get; set; }
+            public int    AI2SearchDepth  { get; set; }
+            public int    AI2ThinkingTime { get; set; }
 
-            [Option(SetName="longhand", Default=3)]
-            public int AI1SearchDepth { get; set; }
-
-            [Option(SetName="longhand", Default=0)]
-            public int AI1MaxThinkingTime { get; set; }
-
-            [Option(SetName="shorthand", Default=null)]
-            public string AI2 { get; set; }
-
-            [Option(SetName="longhand", Default="The Experiment")]
-            public string AI2Name { get; set; }
-
-            [Option(SetName="longhand", Default=3)]
-            public int AI2SearchDepth { get; set; }
-
-            [Option(SetName="longhand", Default=0)]
-            public int AI2MaxThinkingTime { get; set; }
-
-            [Option(Default=5, Required=false)]
-            public int BoardSize { get; set; }
-
-            [Option(Default=1)]
-            public int GameCount { get; set; }
-
-            [Option(Default=0)]
-            public int Seed { get; set; }
-
-            [Option(Default=false)]
-            public bool Force { get; set; }
-
-            [Option(Default=false)]
-            public bool Verbose { get; set; }
+            public int    BoardSize       { get; set; }
+            public int    GameCount       { get; set; }
+            public int    RandomSeed      { get; set; }
+            public bool   Force           { get; set; }
+            public bool   Verbose         { get; set; }
         }
 
 
